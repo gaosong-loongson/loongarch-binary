f7b3e4a935b2f5b087647126fdc702d64ac0ecb6
Song Gao <gaosong@loongson.cn>
2023-12-27 15:46:43 +0800

    update start-qcow2.sh
    Add user and passwd
    
    Signed-off-by: Song Gao <gaosong@loongson.cn>

--- start-qcow2.sh
+++ start-qcow2.sh
@@ -14,3 +14,5 @@
     -device usb-kbd,id=keyboard,bus=xhci.0,port=2 \
     -device virtio-blk-pci,drive=test -drive if=none,id=test,file=./openEuler-22.03-LTS-LoongArch-loongarch64.qcow2 \
     -append "root=/dev/vda2 console=tty  console=ttyS0,115200"
+
+# user: root  passwd: loongson
